71b6ab8f14d8ce378708f2e87cc7b71198b2a294
Xu Chunyang <mail@xuchunyang.me>
2019-05-17 21:55:37 +0800

    Add version to the package header

--- larder.el
+++ larder.el
@@ -6,6 +6,7 @@
 ;; Homepage: https://github.com/xuchunyang/larder.el
 ;; Created: 己亥年 己巳月 癸丑日
 ;; Package-Requires: ((emacs "25.1"))
+;; Version: 0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
